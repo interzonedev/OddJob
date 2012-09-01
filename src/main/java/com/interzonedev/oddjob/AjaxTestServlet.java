fa1025062a28cc3759ffe794d7546d71247948a1
Mark Markarian <mark@interzonedev.com>
2012-08-31 17:32:14 -0700

    Continued implementing response for AjaxTestServlet.java.

--- src/main/java/com/interzonedev/oddjob/AjaxTestServlet.java
+++ src/main/java/com/interzonedev/oddjob/AjaxTestServlet.java
@@ -1,8 +1,15 @@
 package com.interzonedev.oddjob;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,9 +28,8 @@ import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.util.StatusPrinter;
 
 /**
- * Servlet that handles Ajax requests from the OddJob JavaScript unit tests.
- * Returns simple content, content type and status code depending on the
- * parameters in the requeset.
+ * Servlet that handles Ajax requests from the OddJob JavaScript unit tests. Returns simple content, content type and
+ * status code depending on the parameters in the requeset.
  * 
  * @author mmarkarian
  */
@@ -38,9 +44,8 @@ public class AjaxTestServlet extends HttpServlet {
 	private Logger log = (Logger) LoggerFactory.getLogger(getClass());
 
 	/**
-	 * Handles GET requests. Sets the content type and output according to the
-	 * "type" request parameter. Sets the status according to the "error"
-	 * request parameter.
+	 * Handles GET requests. Sets the content type and output according to the "type" request parameter. Sets the status
+	 * according to the "error" request parameter.
 	 * 
 	 * @param request
 	 *            The current {@link HttpServletRequest}
@@ -51,8 +56,7 @@ public class AjaxTestServlet extends HttpServlet {
 	 * @throws IOException
 	 */
 	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		log.debug("doGet: Start");
 
 		processResponseForRequest(request, response);
@@ -61,8 +65,7 @@ public class AjaxTestServlet extends HttpServlet {
 	}
 
 	/**
-	 * Sets the content type and status on the specified response and writes the
-	 * content to the output stream.
+	 * Sets the content type and status on the specified response and writes the content to the output stream.
 	 * 
 	 * @param request
 	 *            The current {@link HttpServletRequest}
@@ -71,8 +74,7 @@ public class AjaxTestServlet extends HttpServlet {
 	 * 
 	 * @throws IOException
 	 */
-	private void processResponseForRequest(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
+	private void processResponseForRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String contentType = getContentTypeForRequest(request);
 		int status = getStatusForRequest(request);
 		String content = getContentForRequest(request);
@@ -83,14 +85,13 @@ public class AjaxTestServlet extends HttpServlet {
 	}
 
 	/**
-	 * Gets the content type for the specified request according to the "type"
-	 * parameter if it is present.
+	 * Gets the content type for the specified request according to the "type" parameter if it is present.
 	 * 
 	 * @param request
 	 *            The current {@link HttpServletRequest}
 	 * 
-	 * @return Returns the content type for the specified request according to
-	 *         the "type" parameter if it is present. Defaults to "text/html";
+	 * @return Returns the content type for the specified request according to the "type" parameter if it is present.
+	 *         Defaults to "text/html";
 	 */
 	private String getContentTypeForRequest(HttpServletRequest request) {
 		String type = request.getParameter("type");
@@ -105,15 +106,13 @@ public class AjaxTestServlet extends HttpServlet {
 	}
 
 	/**
-	 * Gets the status code for the specified request according to the "error"
-	 * parameter if it is present.
+	 * Gets the status code for the specified request according to the "error" parameter if it is present.
 	 * 
 	 * @param request
 	 *            The current {@link HttpServletRequest}
 	 * 
-	 * @return Returns the status code for the specified request according to
-	 *         the "error" parameter if it is present. Defaults to
-	 *         {@link HttpServletResponse#SC_OK}.
+	 * @return Returns the status code for the specified request according to the "error" parameter if it is present.
+	 *         Defaults to {@link HttpServletResponse#SC_OK}.
 	 */
 	private int getStatusForRequest(HttpServletRequest request) {
 		if (isParameterSet(request, "error")) {
@@ -124,14 +123,13 @@ public class AjaxTestServlet extends HttpServlet {
 	}
 
 	/**
-	 * Gets the content for the specified request according to the "type"
-	 * parameter if it is present. Echos the request parameters to the content.
+	 * Gets the content for the specified request according to the "type" parameter if it is present. Echos the request
+	 * parameters to the content.
 	 * 
 	 * @param request
 	 *            The current {@link HttpServletRequest}
 	 * 
-	 * @return Returns the content for the specified request according to the
-	 *         "type" parameter if it is present.
+	 * @return Returns the content for the specified request according to the "type" parameter if it is present.
 	 */
 	private String getContentForRequest(HttpServletRequest request) {
 		String type = request.getParameter("type");
@@ -145,8 +143,7 @@ public class AjaxTestServlet extends HttpServlet {
 		}
 	}
 
-	private boolean isParameterSet(HttpServletRequest request,
-			String parameterName) {
+	private boolean isParameterSet(HttpServletRequest request, String parameterName) {
 		String parameterValue = request.getParameter(parameterName);
 		if ((null != parameterValue) && Boolean.parseBoolean(parameterValue)) {
 			return true;
@@ -158,18 +155,121 @@ public class AjaxTestServlet extends HttpServlet {
 	private String getJsonContent(HttpServletRequest request) {
 		StringBuilder content = new StringBuilder("{");
 
-		content.append("\"method\": \"").append(request.getMethod().toLowerCase()).append("\"");
-		
+		content.append("\"method\":\"").append(request.getMethod().toLowerCase()).append("\",");
+
+		// Echo the request parameters back to the response.
+		Map<String, List<String>> parametersMap = getParametersMap(request);
+		content.append("\"parameters\":[");
+		for (String parameterName : parametersMap.keySet()) {
+			content.append("{");
+			content.append("\"name\":\"").append(parameterName).append("\",");
+			content.append("\"values\":[");
+			List<String> parameterValues = parametersMap.get(parameterName);
+			for (String parameterValue : parameterValues) {
+				content.append("\"").append(parameterValue).append("\",");
+			}
+			if (!parameterValues.isEmpty()) {
+				content.deleteCharAt(content.length() - 1);
+			}
+			content.append("]");
+			content.append("},");
+		}
+		if (!parametersMap.isEmpty()) {
+			content.deleteCharAt(content.length() - 1);
+		}
+		content.append("],");
+
+		// Echo the request headers back to the response.
+		Map<String, List<String>> headersMap = getHeadersMap(request);
+		content.append("\"headers\":[");
+		for (String headerName : headersMap.keySet()) {
+			content.append("{");
+			content.append("\"name\":\"").append(headerName).append("\",");
+			content.append("\"values\":[");
+			List<String> headerValues = headersMap.get(headerName);
+			for (String headerValue : headerValues) {
+				content.append("\"").append(headerValue.replaceAll("\"", "\\\\\"")).append("\",");
+			}
+			if (!headerValues.isEmpty()) {
+				content.deleteCharAt(content.length() - 1);
+			}
+			content.append("]");
+			content.append("},");
+		}
+		if (!headersMap.isEmpty()) {
+			content.deleteCharAt(content.length() - 1);
+		}
+		content.append("],");
+
+		// Echo the request cookies back to the response.
+		Map<String, String> cookiesMap = getCookiesMap(request);
+		content.append("\"cookies\":[");
+		for (String cookieName : cookiesMap.keySet()) {
+			String cookieValue = cookiesMap.get(cookieName);
+			content.append("{");
+			content.append("\"name\":\"").append(cookieName).append("\",");
+			content.append("\"value\":\"").append(cookieValue).append("\"");
+			content.append("},");
+		}
+		if (!cookiesMap.isEmpty()) {
+			content.deleteCharAt(content.length() - 1);
+		}
+		content.append("]");
+
 		content.append("}");
 
 		return content.toString();
 	}
 
 	private String getXmlContent(HttpServletRequest request) {
-		StringBuilder content = new StringBuilder(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+		StringBuilder content = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 
 		content.append("<response method=\"").append(request.getMethod().toLowerCase()).append("\">");
+
+		// Echo the request parameters back to the response.
+		Map<String, List<String>> parametersMap = getParametersMap(request);
+		content.append("<parameters>");
+		for (String parameterName : parametersMap.keySet()) {
+			content.append("<parameter>");
+			content.append("<name>").append("<![CDATA[").append(parameterName).append("]]>").append("</name>");
+			content.append("<values>");
+			List<String> parameterValues = parametersMap.get(parameterName);
+			for (String parameterValue : parameterValues) {
+				content.append("<value>").append("<![CDATA[").append(parameterValue).append("]]>").append("</value>");
+			}
+			content.append("</values>");
+			content.append("</parameter>");
+		}
+		content.append("</parameters>");
+
+		// Echo the request headers back to the response.
+		Map<String, List<String>> headersMap = getHeadersMap(request);
+		content.append("<headers>");
+		for (String headerName : headersMap.keySet()) {
+			content.append("<header>");
+			content.append("<name>").append("<![CDATA[").append(headerName).append("]]>").append("</name>");
+			content.append("<values>");
+			List<String> headerValues = headersMap.get(headerName);
+			for (String headerValue : headerValues) {
+				content.append("<value>").append("<![CDATA[").append(headerValue).append("]]>").append("</value>");
+			}
+			content.append("</values>");
+			content.append("</header>");
+		}
+		content.append("</headers>");
+
+		// Echo the request cookies back to the response.
+		Map<String, String> cookiesMap = getCookiesMap(request);
+		content.append("<cookies>");
+		for (String cookieName : cookiesMap.keySet()) {
+			String cookieValue = cookiesMap.get(cookieName);
+			content.append("<cookie>");
+			content.append("<name>").append("<![CDATA[").append(cookieName).append("]]>").append("</name>");
+			content.append("<value>").append("<![CDATA[").append(cookieValue).append("]]>").append("</value>");
+			content.append("</cookie>");
+		}
+		content.append("</cookies>");
+
 		content.append("</response>");
 
 		return content.toString();
@@ -178,28 +278,107 @@ public class AjaxTestServlet extends HttpServlet {
 	private String getHtmlContent(HttpServletRequest request) {
 		StringBuilder content = new StringBuilder("<div>");
 
-		content.append("<div>");
+		// Output the request method.
+		content.append("<div id=\"method\">");
 		content.append(request.getMethod().toLowerCase());
 		content.append("</div>");
+
+		// Echo the request parameters back to the response.
+		Map<String, List<String>> parametersMap = getParametersMap(request);
+		content.append("<dl id=\"parameters\">");
+		for (String parameterName : parametersMap.keySet()) {
+			content.append("<dt>").append(parameterName).append("</dt>");
+			List<String> parameterValues = parametersMap.get(parameterName);
+			for (String parameterValue : parameterValues) {
+				content.append("<dd>").append(parameterValue).append("</dd>");
+			}
+		}
+		content.append("</dl>");
+
+		// Echo the request headers back to the response.
+		Map<String, List<String>> headersMap = getHeadersMap(request);
+		content.append("<dl id=\"headers\">");
+		for (String headerName : headersMap.keySet()) {
+			content.append("<dt>").append(headerName).append("</dt>");
+			List<String> headerValues = headersMap.get(headerName);
+			for (String headerValue : headerValues) {
+				content.append("<dd>").append(headerValue).append("</dd>");
+			}
+		}
+		content.append("</dl>");
+
+		// Echo the request cookies back to the response.
+		Map<String, String> cookiesMap = getCookiesMap(request);
+		content.append("<dl id=\"cookies\">");
+		for (String cookieName : cookiesMap.keySet()) {
+			content.append("<dt>").append(cookieName).append("</dt>");
+			String cookieValue = cookiesMap.get(cookieName);
+			content.append("<dd>").append(cookieValue).append("</dd>");
+		}
+		content.append("</dl>");
 
 		content.append("</div>");
 
 		return content.toString();
 	}
 
+	private Map<String, List<String>> getParametersMap(HttpServletRequest request) {
+		Map<String, List<String>> parametersMap = new HashMap<String, List<String>>();
+
+		@SuppressWarnings("unchecked")
+		Map<String, String[]> rawParameterMap = request.getParameterMap();
+		for (String parameterName : rawParameterMap.keySet()) {
+			String[] rawParameterValues = rawParameterMap.get(parameterName);
+			List<String> parameterValues = Arrays.asList(rawParameterValues);
+			parametersMap.put(parameterName, parameterValues);
+		}
+
+		return parametersMap;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<String, List<String>> getHeadersMap(HttpServletRequest request) {
+		Map<String, List<String>> headersMap = new HashMap<String, List<String>>();
+
+		for (Enumeration<String> headerNames = request.getHeaderNames(); headerNames.hasMoreElements();) {
+			String headerName = headerNames.nextElement();
+			List<String> headerValues = new ArrayList<String>();
+			for (Enumeration<String> rawHeaderValues = request.getHeaders(headerName); rawHeaderValues
+					.hasMoreElements();) {
+				String headerValue = rawHeaderValues.nextElement();
+				headerValues.add(headerValue);
+			}
+			headersMap.put(headerName, headerValues);
+		}
+
+		return headersMap;
+	}
+
+	private Map<String, String> getCookiesMap(HttpServletRequest request) {
+		Map<String, String> cookiesMap = new HashMap<String, String>();
+
+		Cookie[] cookies = request.getCookies();
+		if (null != cookies) {
+			for (Cookie cookie : cookies) {
+				String cookieName = cookie.getName();
+				String cookieValue = cookie.getValue();
+				cookiesMap.put(cookieName, cookieValue);
+			}
+		}
+
+		return cookiesMap;
+	}
+
 	/**
-	 * Configures the logger context programmatically to avoid an external
-	 * configuration file.
+	 * Configures the logger context programmatically to avoid an external configuration file.
 	 */
 	private static void configureLoggerContext() {
-		LoggerContext loggerContext = (LoggerContext) LoggerFactory
-				.getILoggerFactory();
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		loggerContext.reset();
 
 		// Set log levels on individual packages.
 		loggerContext.getLogger("org.mortbay.log").setLevel(Level.INFO);
-		loggerContext.getLogger("com.interzonedev.oddjob")
-				.setLevel(Level.DEBUG);
+		loggerContext.getLogger("com.interzonedev.oddjob").setLevel(Level.DEBUG);
 
 		String logPattern = "%date [%thread] %-5level %logger - %msg%n";
 		PatternLayoutEncoder patternLayoutEncoder = new PatternLayoutEncoder();
@@ -213,17 +392,15 @@ public class AjaxTestServlet extends HttpServlet {
 		consoleAppender.setEncoder(patternLayoutEncoder);
 		consoleAppender.start();
 
-		loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).addAppender(
-				consoleAppender);
+		loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).addAppender(consoleAppender);
 		loggerContext.start();
 
 		StatusPrinter.printInCaseOfErrorsOrWarnings(loggerContext);
 	}
 
 	/**
-	 * Starts an embedded webserver to serve this servlet. By default the
-	 * servlet will be mapped to {@link #DEFAULT_CONTEXT_PATH}/
-	 * {@link #DEFAULT_SERVLET_MAPPING} on port {@link #DEFAULT_PORT}.
+	 * Starts an embedded webserver to serve this servlet. By default the servlet will be mapped to
+	 * {@link #DEFAULT_CONTEXT_PATH}/ {@link #DEFAULT_SERVLET_MAPPING} on port {@link #DEFAULT_PORT}.
 	 * 
 	 * @param args
 	 * 
--- src/test/javascript/oj.ajax-test.js
+++ src/test/javascript/oj.ajax-test.js
@@ -1,9 +1,18 @@
 $(function() {
-	var ajaxTestServletUrl;
+	var ajaxTestServletUrl, getMethodFromXmlResponse;
+
+	module("oj.ajax");
 
 	ajaxTestServletUrl = "/oddjob/ajaxTest";
 
-	module("oj.ajax");
+	getMethodFromXmlResponse = function(xmlDocument) {
+		var rootNode, method;
+
+		rootNode = xmlDocument.documentElement;
+		method = rootNode.getAttribute("method");
+
+		return method;
+	};
 
 	// oj.ajax object tests
 	test("oj.ajax object defined", function() {
@@ -20,12 +29,13 @@ $(function() {
 		strictEqual(typeof(oj.ajax.doGet), "function", "oj.ajax.doGet is a function");
 	});
 
+	// oj.ajax.doGet method with HTML content type tests
 	test("oj.ajax.doGet function synchronous HTML content type", function() {
-		var response, htmlContent;
+		var response, responseMethodFrag;
 
-		expect(6);
+		expect(8);
 
-		htmlContent = "<div><div>get</div></div>";
+		responseMethodFrag = "<div id=\"method\">get</div>";
 
 		response = oj.ajax.doGet({
 			url: ajaxTestServletUrl,
@@ -34,20 +44,22 @@ $(function() {
 			preventCache: true
 		});
 
-		strictEqual(typeof(response), "object", "oj.ajax.doGet synchronous HTML content type returns an object");
 		strictEqual(response.status, 200, "oj.ajax.doGet synchronous HTML content type returns 200 status");
-		strictEqual(response.content, htmlContent, "oj.ajax.doGet synchronous HTML content type returns test Ajax HTML");
+		strictEqual(typeof(response), "object", "oj.ajax.doGet synchronous HTML content type returns an object");
+		ok(response.content, "oj.ajax.doGet synchronous HTML content type returns content");
+		strictEqual(typeof(response.content), "string", "oj.ajax.doGet synchronous HTML content type returns a string for content");
+		ok(response.content.indexOf(responseMethodFrag) > -1, "oj.ajax.doGet synchronous HTML content type returns the correct method");
 		ok(response.xhr, "oj.ajax.doGet synchronous HTML content type returns the XHR object");
 		strictEqual(typeof(response.xhr), "object", "oj.ajax.doGet synchronous HTML content type returns the XHR object");
 		strictEqual(response.xhr.status, 200, "oj.ajax.doGet synchronous HTML content type returns the XHR object");
 	});
 
 	asyncTest("oj.ajax.doGet function asynchronous HTML content type with valid arguments", function() {
-		var synchronousResponse, htmlContent;
+		var synchronousResponse, responseMethodFrag;
 
-		expect(6);
+		expect(8);
 
-		htmlContent = "<div><div>get</div></div>";
+		responseMethodFrag = "<div id=\"method\">get</div>";
 
 		synchronousResponse = oj.ajax.doGet({
 			url: ajaxTestServletUrl,
@@ -55,8 +67,10 @@ $(function() {
 			asynchronous: true,
 			preventCache: true,
 			successCallback: function(response, status, xhr) {
-				strictEqual(response, htmlContent, "oj.ajax.doGet asynchronous HTML content type with valid arguments returns test Ajax HTML");
 				strictEqual(status, 200, "oj.ajax.doGet asynchronous HTML content type with valid arguments returns 200 status");
+				ok(response, "oj.ajax.doGet asynchronous HTML content type with valid arguments returns content");
+				strictEqual(typeof(response), "string", "oj.ajax.doGet asynchronous HTML content type with valid arguments returns a string");
+				ok(response.indexOf(responseMethodFrag) > -1, "j.ajax.doGet asynchronous HTML content type with valid arguments returns the correct method");
 				ok(xhr, "oj.ajax.doGet asynchronous HTML content type with valid arguments returns the XHR object");
 				strictEqual(typeof(xhr), "object", "oj.ajax.doGet asynchronous HTML content type with valid arguments returns the XHR object");
 				strictEqual(xhr.status, 200, "oj.ajax.doGet asynchronous HTML content type with valid arguments returns the XHR object");
@@ -72,14 +86,11 @@ $(function() {
 		strictEqual(synchronousResponse, null, "oj.ajax.doGet asynchronous HTML content type with valid arguments returns null");
 	});
 
+	// oj.ajax.doGet method with JSON content type tests
 	asyncTest("oj.ajax.doGet function asynchronous JSON content type with valid arguments", function() {
-		var synchronousResponse, jsonContent;
+		var synchronousResponse;
 
-		expect(6);
-
-		jsonContent = {
-			"method": "get"
-		};
+		expect(8);
 
 		synchronousResponse = oj.ajax.doGet({
 			url: ajaxTestServletUrl + "?type=json",
@@ -87,8 +98,10 @@ $(function() {
 			asynchronous: true,
 			preventCache: true,
 			successCallback: function(response, status, xhr) {
-				deepEqual(response, jsonContent, "oj.ajax.doGet asynchronous JSON content type with valid arguments returns test Ajax JSON");
 				strictEqual(status, 200, "oj.ajax.doGet asynchronous JSON content type with valid arguments returns 200 status");
+				ok(response, "oj.ajax.doGet asynchronous JSON content type with valid arguments returns content");
+				strictEqual(typeof(response), "object", "oj.ajax.doGet asynchronous JSON content type with valid arguments returns an object");
+				strictEqual(response.method, "get", "oj.ajax.doGet asynchronous JSON content type with valid arguments returns test Ajax JSON");
 				ok(xhr, "oj.ajax.doGet asynchronous JSON content type with valid arguments returns the XHR object");
 				strictEqual(typeof(xhr), "object", "oj.ajax.doGet asynchronous JSON content type with valid arguments returns the XHR object");
 				strictEqual(xhr.status, 200, "oj.ajax.doGet asynchronous JSON content type with valid arguments returns the XHR object");
@@ -102,5 +115,41 @@ $(function() {
 		});
 
 		strictEqual(synchronousResponse, null, "oj.ajax.doGet asynchronous JSON content type with valid arguments returns null");
+	});
+
+	// oj.ajax.doGet method with XML content type tests
+	asyncTest("oj.ajax.doGet function asynchronous XML content type with valid arguments", function() {
+		var synchronousResponse;
+
+		expect(8);
+
+		synchronousResponse = oj.ajax.doGet({
+			url: ajaxTestServletUrl + "?type=xml",
+			params: {},
+			asynchronous: true,
+			preventCache: true,
+			successCallback: function(response, status, xhr) {
+				var method;
+
+				strictEqual(status, 200, "oj.ajax.doGet asynchronous XML content type with valid arguments returns 200 status");
+				ok(response, "oj.ajax.doGet asynchronous XML content type with valid arguments returns content");
+				strictEqual(Object.prototype.toString.apply(response), "[object XMLDocument]", "oj.ajax.doGet asynchronous XML content type with valid arguments returns an XML object");
+
+				method = getMethodFromXmlResponse(response);
+				strictEqual(method, "get", "j.ajax.doGet asynchronous XML content type with valid arguments returns the correct method");
+
+				ok(xhr, "oj.ajax.doGet asynchronous XML content type with valid arguments returns the XHR object");
+				strictEqual(typeof(xhr), "object", "oj.ajax.doGet asynchronous XML content type with valid arguments returns the XHR object");
+				strictEqual(xhr.status, 200, "oj.ajax.doGet asynchronous XML content type with valid arguments returns the XHR object");
+
+				start();
+			},
+			errorCallback: function(response, status, xhr) {
+				ok(false, "oj.ajax.doGet asynchronous XML content type with valid arguments should not call the error callback ");
+				start();
+			}
+		});
+
+		strictEqual(synchronousResponse, null, "oj.ajax.doGet asynchronous XML content type with valid arguments returns null");
 	});
 });
